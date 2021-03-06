960baafdb179ff3328294a7b08c338a351b43a7b
Manuel Echeverri <manuelecheverrir@yahoo.com>
2021-03-06 10:07:29 -0500

    Commit incial

---
+++ apiestudiante2021/src/test/java/co/com/edu/apiestudiante2021/Apiestudiante2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.apiestudiante2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apiestudiante2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
